ebf571de85552e623bca21011f9f44970286bd51
Cameron <cameronjmaccabe@gmail.com>
2019-09-19 13:21:39 -0700

    added video and photo functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,39 @@
 const video = document.querySelector(".player");
 const canvas = document.querySelector(".photo");
-const ctx = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(e => console.error(e));
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    context.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  const link = document.createElement("a");
+  link.href = canvas.toDataURL("image/jpeg");
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${link.href}" alt="Handsome">`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
